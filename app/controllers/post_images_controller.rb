42183d22c442fce8154b9e13bd917909f53a6e3d
yuto-takami <orca.y1322@gmail.com>
2021-06-18 13:37:51 +0000

    [Finish]4-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,11 +11,13 @@ class PostImagesController < ApplicationController
   end
   
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
+    
   end
   
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
   
   def destroy
@@ -25,6 +27,7 @@ class PostImagesController < ApplicationController
   end
   
   private
+  
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
