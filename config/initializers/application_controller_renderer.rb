fc837d127b563b1b7581e1889055f29112136b0e
yuto-takami <orca.y1322@gmail.com>
2021-06-18 08:51:10 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '462e96331a2b3ce2ddfd41e77b3c4bd1dba7b858ef923a1cee2e3702667d43d3cdc315b06afdf52aa84bb9068fc8a98f5e4558bcf54c9614191fe9b8f505b0a7'
